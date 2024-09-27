8c71042d4324ae28e6b29c4101cd51b19cffd61c
Allertvdw <allertvdw@hotmail.com>
2024-09-27 13:28:33 +0200

    Added some booking logic

---
+++ KartAppBE/KartAppBE.DAL/Repositories/BookingRepository.cs
@@ -0,0 +1,21 @@
+﻿using KartAppBE.BLL.Models;
+using KartAppBE.BLL.Interfaces.Repositories;
+using KartAppBE.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartAppBE.DAL.Repositories
+{
+	public class BookingRepository(ApplicationDbContext dbContext) : IBookingRepository
+	{
+		public Booking? Create(Booking booking)
+		{
+			dbContext.Bookings.Add(booking);
+
+			return dbContext.SaveChanges() > 0 ? booking : null;
+		}
+	}
+}
