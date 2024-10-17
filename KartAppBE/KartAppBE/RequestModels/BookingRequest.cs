95d03712ce61f031083c131e5a64b3d119e1a954
Allertvdw <allertvdw@hotmail.com>
2024-10-17 12:50:59 +0200

    Added email and phone number to endpoint

--- KartAppBE/KartAppBE/RequestModels/BookingRequest.cs
+++ KartAppBE/KartAppBE/RequestModels/BookingRequest.cs
@@ -4,5 +4,7 @@
 	{
 		public int SessionId { get; set; }
 		public int PeopleCount { get; set; }
+		public string Email { get; set; } = string.Empty;
+		public string PhoneNumber { get; set; } = string.Empty;
 	}
 }
